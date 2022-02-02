17b62a308a7860bbab006fd37f19a71a360bd4fb
Anslate <1959719289@qq.com>
2022-02-03 04:19:30 +0800

    alpha02032022
    算法改动，DotAP文件语法改动，删除的矩形形状的动画

--- Debug/Main.py
+++ Debug/Main.py
@@ -1,8 +1,7 @@
+#运行时会跳出一次未知命令,这是正常现象,是我偷懒在处理普通命令时没有跳过文件头,把文件头认为普通命令处理从而导致的未知
 from PIL import Image,ImageDraw
 image = []
 image_draw = []
-
-
 
 DotAP_file = open("./DotAP.txt", "r", encoding="utf-8")#读取文件
 DotAP_file = DotAP_file.read()#还是读取
@@ -14,47 +13,32 @@ for i in range(int(Header[0])):
     image.append(Image.new("L", (int(Header[1]),int(Header[2])), int(Header[3])))#创建场景
     image_draw.append(ImageDraw.Draw(image[i]))#转换为ImageDraw对象
 
-#接下来开始绘制
-count = -1 #此变量用来进行如下循环的计数
+class point:
+    def __init__(self,start_frame,x,y,color):
+        self.frame = start_frame-1#这个frame是待绘制的帧，此帧还没绘制！！！减1是因为计算机从0开始数而人类从1开始数
+        self.x = x
+        self.y = y
+        self.color = color
+    def still(self,frames):#frames是帧数，注意单复数
+        for i in range(frames):
+            image_draw[self.frame].point((self.x,self.y), self.color)
+            self.frame += 1#绘制完成之后将待绘制帧移到下一位
+    def move(self,x2,y2,step):
+        pass
+
+objects = {}
 for command in DotAP_list:
-    if(count == -1):#跳过第一次循环
-        count += 1
-        continue
-    command = command.split(" ")#将DotAP语句储存于命令(command)
-    #区分静止与否
-    if(command[0] == "still"):
-        if(command[1] == "point"):#绘制点(point)
-            for i in range(int(command[3])):#帧持续长度,偷个小懒用i表示了,因为DotAP中开始帧1帧表示为1而不是编程中常用的0,所以下方还要减去1
-                image_draw[int(command[2])+i-1].point((int(command[4]),int(command[5])), int(command[6]))
-        elif(command[1] == "rectangle"):#绘制矩形(retangle)
-            for i in range(int(command[3])):
-                image_draw[int(command[2])+i-1].rectangle((int(command[4]),int(command[5]),int(command[6]),int(command[7])), int(command[10]), int(command[9]), int(command[8]))
+    command = command.split(" ")
+    if(command[0] == "create"):#由于Python不支持switch...case语句,于是用if...elif...else取代
+        if(command[1] == "point"):
+            objects[command[2]] = point(int(command[3]), int(command[4]), int(command[5]), int(command[6]))
         else:
-            print("未知命令，在第",str(count+2),"行，第 2 项")
-            break
-    elif(command[0] == "move"):
-        if(command[3] == "1"):
-            print("运动图形持续帧数不能为1，在",str(count+2),"行")
-            break
-        if(command[1] == "point"):#绘制动点(point)
-            step_x = (int(command[6]) - int(command[4]))/(int(command[3])-1)#计算x轴每次移动步长
-            step_y = (int(command[7]) - int(command[5]))/(int(command[3])-1)#计算y轴每次移动步长
-            for i in range(int(command[3])):#同上
-                image_draw[int(command[2])+i-1].point((int(command[4])+step_x*i,int(command[5])+step_y*i), int(command[8]))
-        elif(command[1] == "rectangle"):
-            step_x1 = (int(command[8]) - int(command[4]))/(int(command[3])-1)
-            step_y1 = (int(command[9]) - int(command[5]))/(int(command[3])-1)
-            step_x2 = (int(command[10]) - int(command[6]))/(int(command[3])-1)
-            step_y2 = (int(command[11]) - int(command[7]))/(int(command[3])-1)
-            for i in range(int(command[3])):
-                image_draw[int(command[2])+i-1].rectangle((int(command[4])+step_x1*i,int(command[5])+step_y1*i,int(command[6])+step_x2*i,int(command[7])+step_y2*i), int(command[14]), int(command[13]), int(command[12]))
-        else:
-            print("未知命令，在第",str(count+2),"行，第 2 项")
-            break
+            print("未知命令")
     else:
-        print("未知命令，在第",str(count+2)," 行，第 1 项")
-    count+=1
+        if(command[1]=="still"):
+            objects[command[0]].still(int(command[2]))
+        else:
+            print("未知命令")
 
 for i in image:
     i.show()
-    pass
