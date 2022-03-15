ae199f46d184c94a66c6a731560a6be7ca226e23
Anslate <1959719289@qq.com>
2022-03-16 02:43:47 +0800

    日常更改

--- Debug/Main.py
+++ Debug/Main.py
@@ -1,3 +1,5 @@
+#这是我自己用的工具，纠错聊胜于无！
+from turtle import width
 from PIL import Image,ImageDraw
 if(__name__ == "__main__"):
     DotAP_text = open("./Debug/DotAP.txt","r")
@@ -18,26 +20,22 @@ if(__name__ == "__main__"):
         Images.append(Image.new("RGB",(header[1],header[2]),tuple(backgcolor)))
         ImagesD.append(ImageDraw.Draw(Images[i],"RGB"))
 
-    class basic():
+    def list_change(stri):#字符逗号分割的字符串改成整形数组
+        lis = []
+        stri = stri.split(",")
+        for i in stri:
+            lis.append(int(i))
+        return lis
+
+
+    class basic():#标准图形至少三个坐标，少于三个的需转换为其他图形
         def __init__(self,position,fill_color,outline_color,width):
-            #将坐标转为二维数组
-            position=position.split(",")
-            self.position=[]
-            for i in range(0,len(position),2):
-                self.position.append([int(position[i]),int(position[i+1])])
-            #填充颜色的转换
-            self.fill_color=[]
-            fill_color=fill_color.split(",")
-            for i in fill_color:
-                self.fill_color.append(int(i))
-            #轮廓颜色转换
-            self.outline_color=[]
-            outline_color=outline_color.split(",")
-            for i in outline_color:
-                self.outline_color.append(int(i))
-            #线宽
+            self.position=position
+            self.fill_color=list_change(fill_color)
+            self.outline_color=list_change(outline_color)
             self.width = int(width)
         def show(self,startf,stopf):
+            #Image库只接受元组，但图形动起来时数据肯定会变，所以就在这改元组了（毕竟都用Py了，慢点就慢点）
             #坐标改元组
             position = []
             for i in self.position:
@@ -45,23 +43,61 @@ if(__name__ == "__main__"):
             #颜色改元组
             fill_color = tuple(self.fill_color)
             outline_color = tuple(self.outline_color)
-            for i in range((stopf-startf)+1):
+            for i in range(stopf-startf+1):
                 ImagesD[startf-1+i].polygon(position,fill_color,outline_color,self.width)
 
+    class line():#标准线段至少两个坐标，少于两个的需转换为其他图形
+        def __init__(self,position,outline_color,width):
+            self.position = position
+            self.outline_color=list_change(outline_color)
+            self.width = int(width)
+        def show(self,startf,stopf):
+            #改元组
+            position = []
+            for i in self.position:
+                position.append(tuple(i))
+            outline_color = tuple(self.outline_color)
+            for i in range(stopf-startf+1):
+                ImagesD[startf-1+i].line(position,outline_color,self.width)
+
+
+    class point():
+        pass
+
     graph = {}
-    for line in range(1,len(DotAP_command),1):
-        command = DotAP_command[line].split(" ")
+    for lines in range(1,len(DotAP_command),1):
+        command = DotAP_command[lines].split(" ")
         if(command[0]=="create"):
             if(command[1]=="basic"):
-                graph[command[2]] = basic(command[3],command[4],command[5],command[6])
+                #将坐标转为二维数组，position是一维数组，position_list是二维数组
+                position=command[3].split(",")
+                position_list=[]
+                for i in range(0,len(position),2):
+                    position_list.append([int(position[i]),int(position[i+1])])
+                #转换为图形对象
+                #大于等于3个坐标的是basic标准图形  
+                #2个是线段，1个是点，此时填充颜色作废 
+                if(len(position_list)>2):#三个坐标
+                    graph[command[2]] = basic(position_list,command[4],command[5],command[6])
+                elif(len(position_list)==2):#两个坐标
+                    graph[command[2]] = line(position_list,command[5],command[6])
+                else:#1个坐标或负个坐标(虽然不可能，但万一有时能防报错)，如果是0个坐标那直接未知命令了
+                    pass
+            elif(command[1]=="line"):
+                #将坐标转为二维数组，position是一维数组，position_list是二维数组
+                position=command[3].split(",")
+                position_list=[]
+                for i in range(0,len(position),2):
+                    position_list.append([int(position[i]),int(position[i+1])])
+                graph[command[2]] = line(position_list,command[4],command[5])
             else:
-                print("未知命令，在",line+1,"行，第 2 段")
+                print("未知命令，在",lines+1,"行，第 2 段")
         elif(command[0]=="show"):
             graph[command[1]].show(int(command[2]),int(command[3]))
         elif(command[0]=="move"):
             pass
         else:
-            print("未知命令，在",line+1,"行，第 1 段")
+            print("未知命令，在",lines+1,"行，第 1 段")
 
     for i in Images:
         i.show()
