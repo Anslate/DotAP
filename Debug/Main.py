1f5f84b96140118c156ad12f1c83e1254f083670
Anslate <1959719289@qq.com>
2022-03-19 12:59:19 +0800

    小改动

--- Debug/Main.py
+++ Debug/Main.py
@@ -1,5 +1,7 @@
 #这是我自己用的工具，纠错聊胜于无！
-from turtle import width
+#几个容易混淆的数据:
+#ImagesD[]是ImagesDraw对象，以帧数排序,[帧1，帧2]
+#graph{}字典，储存着图形对象（我自定义的类）和标识名,{表示名:图形对象}
 from PIL import Image,ImageDraw
 if(__name__ == "__main__"):
     DotAP_text = open("./Debug/DotAP.txt","r")
@@ -27,6 +29,24 @@ if(__name__ == "__main__"):
             lis.append(int(i))
         return lis
 
+    def to_2list(position):#将坐标转为二维数组，position是一维数组，position_list是二维数组
+        position=position.split(",")
+        position_list=[]
+        for i in range(0,len(position),2):
+            position_list.append([int(position[i]),int(position[i+1])])
+        return position_list
+
+    class group():#group是编组，简而言之，批量操作
+        def __init__(self,items,graph):
+            self.items = items.split(",")
+            self.graph = graph
+        def show(self,startf,stopf):
+            for i in self.items:
+                if(i in graph):
+                    self.graph[i].show(startf,stopf)
+                else:
+                    print(i,"未创建")
+
 
     class basic():#标准图形至少三个坐标，少于三个的需转换为其他图形
         def __init__(self,position,fill_color,outline_color,width):
@@ -45,6 +65,22 @@ if(__name__ == "__main__"):
             outline_color = tuple(self.outline_color)
             for i in range(stopf-startf+1):
                 ImagesD[startf-1+i].polygon(position,fill_color,outline_color,self.width)
+        def move_to_position(self,position,startf,stopf):
+            #计算步长
+            step = []#step是二维数组
+            frames = stopf-startf+1#包括两个断点，所以加1（例：1到3帧需要显示1，2，3帧，但3-1=2，frames只有2帧，所以要加1）
+            for i in range(len(position)):
+                step.append([])
+                for j in range(2):
+                    step[i].append((position[i][j]-self.position[i][j])/frames)
+            #绘制
+            for i in range(frames):
+                for j in range(len(position)):
+                    for k in range(2):
+                        self.position[j][k] = self.position[j][k] + step[j][k]*i
+                self.show(startf+i,startf+i)
+
+
 
     class line():#标准线段至少两个坐标，少于两个的需转换为其他图形
         def __init__(self,position,outline_color,width):
@@ -64,38 +100,31 @@ if(__name__ == "__main__"):
     class point():
         pass
 
-    graph = {}
+    graph = {}#哈希表，可存储图形对象（basic,line等）和编组对象
     for lines in range(1,len(DotAP_command),1):
         command = DotAP_command[lines].split(" ")
-        if(command[0]=="create"):
+        if(command[0]=="group"):
+            graph[command[1]] = group(command[2],graph)
+        elif(command[0]=="create"):
+            position_2list = to_2list(command[3])
             if(command[1]=="basic"):
-                #将坐标转为二维数组，position是一维数组，position_list是二维数组
-                position=command[3].split(",")
-                position_list=[]
-                for i in range(0,len(position),2):
-                    position_list.append([int(position[i]),int(position[i+1])])
-                #转换为图形对象
                 #大于等于3个坐标的是basic标准图形  
                 #2个是线段，1个是点，此时填充颜色作废 
-                if(len(position_list)>2):#三个坐标
-                    graph[command[2]] = basic(position_list,command[4],command[5],command[6])
-                elif(len(position_list)==2):#两个坐标
-                    graph[command[2]] = line(position_list,command[5],command[6])
+                if(len(position_2list)>2):#三个坐标
+                    graph[command[2]] = basic(position_2list,command[4],command[5],command[6])
+                elif(len(position_2list)==2):#两个坐标
+                    graph[command[2]] = line(position_2list,command[5],command[6])
                 else:#1个坐标或负个坐标(虽然不可能，但万一有时能防报错)，如果是0个坐标那直接未知命令了
                     pass
             elif(command[1]=="line"):
-                #将坐标转为二维数组，position是一维数组，position_list是二维数组
-                position=command[3].split(",")
-                position_list=[]
-                for i in range(0,len(position),2):
-                    position_list.append([int(position[i]),int(position[i+1])])
-                graph[command[2]] = line(position_list,command[4],command[5])
+                graph[command[2]] = line(position_2list,command[4],command[5])
             else:
                 print("未知命令，在",lines+1,"行，第 2 段")
         elif(command[0]=="show"):
             graph[command[1]].show(int(command[2]),int(command[3]))
         elif(command[0]=="move"):
-            pass
+            position_2list = to_2list(command[2])
+            graph[command[1]].move_to_position(position_2list,int(command[3]),int(command[4]))
         else:
             print("未知命令，在",lines+1,"行，第 1 段")
 
